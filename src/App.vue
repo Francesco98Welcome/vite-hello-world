a1fba43f303a1bfc65b8a3212f2dbd83aed405a8
Francesco98Welcome <frankbenvenuti04@gmail.com>
2023-02-01 16:22:30 +0100

    fine primo es. vite

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from "./components/AppTitle.vue";
+export default {
+  components: {
+    AppTitle,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      message: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>
+    {{ message }}
+  </h2>
+</template>
+
+<style scoped>
+
+</style>
